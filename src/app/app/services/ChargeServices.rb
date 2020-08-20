89c2734b2a45230dc136a380fefaebcc89defd77
yoshi0202 <kobayashi020202@gmail.com>
2020-08-21 08:29:04 +0900

    fix: create chargedto methods

--- src/app/app/services/ChargeServices.rb
+++ src/app/app/services/ChargeServices.rb
@@ -8,16 +8,12 @@ class ChargeServices
   def create_chargeDTO
     utility_charge = UtilityCharge.first
     charge_dto = ChargeDTO.new
-    water_dto = WaterServices.new.create_dto(1000)
-    electricity_dto = ElectricityServices.new.create_dto(1000)
-    gas_dto = GasServices.new.create_dto(1000)
-    mobile_phone_DTO = MobilePhoneServices.new.create_dto(1999)
     charge_dto.id = utility_charge.id
 
-    charge_dto.water = water_dto
-    charge_dto.electricity = electricity_dto
-    charge_dto.mobile_phone = mobile_phone_DTO
-    charge_dto.gas = gas_dto
+    charge_dto.water = WaterServices.new.create_dto(1000)
+    charge_dto.electricity = ElectricityServices.new.create_dto(1000)
+    charge_dto.mobile_phone = MobilePhoneServices.new.create_dto(1999)
+    charge_dto.gas = GasServices.new.create_dto(1000)
 
     return charge_dto
   end

ddee8b1168fec415a9cb52b8fc0ba9a1c73ad788
yoshi0202 <kobayashi020202@gmail.com>
2020-08-21 07:55:36 +0900

    add: test files

---
+++ src/app/test/controllers/utility_charge/electricity_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UtilityCharge::ElectricityControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ src/app/test/controllers/utility_charge/water_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UtilityCharge::WaterControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

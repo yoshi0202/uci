3f1c06a3f73541f43cff6ddad01dd899d6eec162
yoshi0202 <kobayashi020202@gmail.com>
2020-08-22 20:15:20 +0900

    add: new routes

--- src/app/config/routes.rb
+++ src/app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/utility_charge', to: 'utility_charges#index'
+  get '/utility_charge/new', to: 'utility_charges#new'
+  get '/utility_charge/show', to: 'utility_charges#show'
+  post '/utility_charge/create', to: 'utility_charges#create'
   namespace :utility_charge do
     get '/water', to: 'water#index'
     get '/electricity', to: 'electricity#index'

004bb2fa419c772ae72a27c857e7fd234be61c86
yoshi0202 <kobayashi020202@gmail.com>
2020-08-22 20:14:14 +0900

    add: seed file

--- src/app/db/seeds.rb
+++ src/app/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+for i in 1..12 do
+  month = i.to_s.rjust(2, "0")
+  util = UtilityCharge.create({
+    created_at: "2020-" + month + "-01 00:00:00",
+    updated_at: "2020-" + month + "-01 00:00:00",
+  })
+  ElectricitySupply.create({
+    utility_charge_id: util.id,
+    cost: 1000 * i,
+    created_at: "2020-" + month + "-01 00:00:00",
+    updated_at: "2020-" + month + "-01 00:00:00",
+  })
+
+  GasSupply.create({
+    utility_charge_id: util.id,
+    cost: 1000 * i,
+    created_at: "2020-" + month + "-01 00:00:00",
+    updated_at: "2020-" + month + "-01 00:00:00",
+  })
+
+  MobilePhoneSupply.create({
+    utility_charge_id: util.id,
+    cost: 1000 * i,
+    created_at: "2020-" + month + "-01 00:00:00",
+    updated_at: "2020-" + month + "-01 00:00:00",
+  })
+
+  WaterSupply.create({
+    utility_charge_id: util.id,
+    cost: 1000 * i,
+    created_at: "2020-" + month + "-01 00:00:00",
+    updated_at: "2020-" + month + "-01 00:00:00",
+  })
+end
